42d48fe3248cc6b30293f37210d85d28a324ac64
todorr92 <todorovicslobodan10@gmail.com>
2020-08-06 21:51:35 +0000

    added index.html

--- run.py
+++ run.py
@@ -1,35 +1,37 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
 
 
 def add_messages(username, message):
-    """Add messages to 'messages' list"""
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
-    """Get all messages and seperate them with 'br'"""
+    """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
 
 @app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0} </h1> {1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    """Create new message and redirect back to the chat page"""
+    """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
 
